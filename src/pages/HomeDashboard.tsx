7638e3a9fd6316fbcd3b804fa3ea6e0ad6018ef4
Juan Viveros <juan.viveros@trob.com.mx>
2026-04-12 01:53:50 -0600

    V27j: forzar KPI y subtitle textAlign left

--- src/pages/HomeDashboard.tsx
+++ src/pages/HomeDashboard.tsx
@@ -171,8 +171,7 @@ export default function HomeDashboard() {
           fontSize: '28px',
           fontWeight: 600,
           color: '#FFFFFF',
-          textAlign: 'left',
-          lineHeight: 1,
+          textAlign: 'left', width: '100%',lineHeight: 1,
           marginTop: '6px',
           position: 'relative',
           zIndex: 1,
@@ -231,6 +230,7 @@ export default function HomeDashboard() {
     </div>
   )
 }
+
 
 
 
